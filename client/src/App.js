02fd165801a8ff265888683770ccc14f8631c3dc
Anvesh Rai <127948099+ARnord-Trigger@users.noreply.github.com>
2025-07-21 18:02:37 +0530

    modiifed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {DataProvider} from "./GlobalState"
+import Header from './components/header/Header';
+import Pages from "./components/MainPage/Pages"
+import Footer from './components/footer/Footer';
+import ScrollTop from './components/MainPage/support/ScrollTop';
+import ScrollToTop from './components/MainPage/support/ScrollTopPage';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataProvider>
+      <Router>
+          <ScrollToTop/>
+          <Header/>
+             <Pages/>
+          <ScrollTop/>
+          <Footer/>
+      </Router>
+      </DataProvider>
     </div>
   );
 }
